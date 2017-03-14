3b5cec1655384af4280c749d9c58f1fc020583c9
mearichaitali <chaitali.jellynut@gmail.com>
2017-03-14 17:44:53 +0530

    Title changed..
    Title changed..

--- test/index.php
+++ test/index.php
@@ -1,5 +1,14 @@
-<?php
-
-echo "Hello...";
-
+<?php 
+$title = "Veinsa";
 ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php echo $title;?></title>
+</head>
+<body>
+	<h2><b>hello this is testing page</b></h2>
+</body>
+</html>
